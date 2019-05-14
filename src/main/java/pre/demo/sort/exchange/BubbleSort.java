b05e0851774f04f308b53b03b8402c1c512c4e43
Yang.Yu <Yang.Yu@123.com>
2019-05-14 15:22:18 +0800

    冒泡排序 优化

--- src/main/java/pre/demo/sort/exchange/BubbleSort.java
+++ src/main/java/pre/demo/sort/exchange/BubbleSort.java
@@ -2,6 +2,8 @@ package pre.demo.sort.exchange;
 
 /**
  * 交换排序之——冒泡排序.
+ *
+ * 2019-05-04 优化，增加didSwap，将最好情况时间复杂度降为O(n)
  * @author Yang.Yu 2018/5/14 10:28
  */
 public class BubbleSort {
@@ -14,14 +16,23 @@ public class BubbleSort {
   }
 
   private static void bsort(int[] arr) {
-    int tmp;
+    boolean didSwap;
+
     for (int j = arr.length; j >= 0; j--) {
+      didSwap = false;
+
       for (int i = 0; i < j - 1; i++) {
         if (arr[i] > arr[i + 1]) {
+          int tmp;
           tmp = arr[i + 1];
           arr[i + 1] = arr[i];
           arr[i] = tmp;
+          didSwap = true;
         }
+      }
+
+      if (!didSwap) {
+        return;
       }
     }
   }
